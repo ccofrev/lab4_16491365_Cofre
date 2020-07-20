8ba88d931042f965e94555479a7edbc4b35bf905
Carlos Cofré <carlos.cofre@usach.cl>
2020-07-20 04:38:26 -0400

    Clase Archivo

--- Lab3/src/lab3/Lab3.java
+++ Lab3/src/lab3/Lab3.java
@@ -4,7 +4,7 @@
  * 
  */
 package lab3;
-
+import java.time.LocalDateTime; 
 /**
  *
  * @author ccofr
@@ -15,7 +15,8 @@ public class Lab3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Archivo ar1 = new Archivo("ar1", LocalDateTime.now(), "hola\nEste es un archivo de texto\nfin" );
+        System.out.println("Archivo:\n" + ar1.toString());
     }
     
 }
